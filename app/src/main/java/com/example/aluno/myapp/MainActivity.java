ed7388d1c7371bb2e6f19452defd144464b14df6
Aluno <a>
2017-09-26 09:20:34 -0300

    a

--- app/src/main/java/com/example/aluno/myapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapp/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.aluno.myapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button botao;
+    private LocationManager a;
+    private LocationListener b;
+    private LocationProvider c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        botao = (Button) findViewById(R.id.botao);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                a.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                Location l= new Location(String.valueOf(a));
+
+                Toast.makeText(MainActivity.this," "+l.getLongitude()+"     "+l.getLatitude(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        a = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        b = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                String g="/n"+ location.getLatitude() + ""+location.getLongitude();
+                Toast.makeText(MainActivity.this, g, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+    }}
+
+
